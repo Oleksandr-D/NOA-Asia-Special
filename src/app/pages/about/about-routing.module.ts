9a4add189d07d9f4ed59702bbfa2aa72d6087857
Oleksandr-D <dolbnja9@gmail.com>
2023-03-31 12:15:37 +0300

    added routes to header

--- src/app/pages/about/about-routing.module.ts
+++ src/app/pages/about/about-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from '../../admin/admin.component';
+import { AboutComponent } from './about.component';
 
 
-const routes: Routes = [ { path: '', component: AdminComponent } ];
+const routes: Routes = [ { path: '', component: AboutComponent } ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
