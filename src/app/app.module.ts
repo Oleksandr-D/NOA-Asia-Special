86ce0478b71ba6151e43472dcf9e1ff872e15f97
Oleksandr-D <dolbnja9@gmail.com>
2023-04-04 10:58:27 +0300

    added angular material, contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
+const MATERIAL = [
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 
 @NgModule({
   declarations: [],
-  imports: [ CommonModule, FormsModule, ReactiveFormsModule ],
-  exports: [ FormsModule, ReactiveFormsModule ],
+  imports: [MATERIAL, FormsModule, ReactiveFormsModule],
+  exports: [MATERIAL, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule { }
