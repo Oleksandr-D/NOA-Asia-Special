aece0a2826a27fc3e153fa3483f9bf136bba1534
Oleksandr-D <dolbnja9@gmail.com>
2023-04-04 16:49:30 +0300

    added material color theme, mat form at contact component

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  public questionForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  initAuthForm(): void {
+    this.questionForm = this.fb.group({
+      name: [null, [Validators.required,]],
+      surname:[null, [Validators.required]],
+      tel:[null, [Validators.required]],
+      email:[null, [Validators.required, Validators.email]],
+      message:[null, [Validators.required]],
+     
+    });
+  }
+
+
+
+
+
 
 }
