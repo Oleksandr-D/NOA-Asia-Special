755da60439f8446a22d650fef2f4c24d41a3253b
Oleksandr-D <dolbnja9@gmail.com>
2023-04-05 10:28:49 +0300

    added content at about component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
