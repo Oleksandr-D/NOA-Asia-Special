d804d6d44100a4df2947e660a50d083cb566a875
Oleksandr-D <dolbnja9@gmail.com>
2023-04-10 11:51:25 +0300

    added route active, components to vacansies, banner at kuhar-ramen component

--- src/app/pages/vacancies/vacancies-router.module.ts
+++ src/app/pages/vacancies/vacancies-router.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VacanciesComponent } from './vacancies.component';
+import { KuharWokComponent } from '../kuhar-wok/kuhar-wok.component';
+import { SushystComponent } from '../sushyst/sushyst.component';
+import { KyharRamenComponent } from '../kyhar-ramen/kyhar-ramen.component';
+import { BarmenComponent } from '../barmen/barmen.component';
 
-const routes: Routes = [ { path: '', component: VacanciesComponent } ];
+const routes: Routes = [
+  { path: '', component: VacanciesComponent},
+  { path: 'kyhar-wok', component: KuharWokComponent },
+  { path: 'sychyst', component: SushystComponent},
+  { path: 'kyhar-ramen', component: KyharRamenComponent },
+  { path: 'barmen', component: BarmenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/vacancies/vacancies.module.ts
+++ src/app/pages/vacancies/vacancies.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { VacanciesComponent } from './vacancies.component';
 import { VacanciesRouterModule } from './vacancies-router.module';
 import { SharedModule } from '../../shared/shared.module';
+import { KuharWokComponent } from '../kuhar-wok/kuhar-wok.component';
+import { BarmenComponent } from '../barmen/barmen.component';
+import { KyharRamenComponent } from '../kyhar-ramen/kyhar-ramen.component';
+import { SushystComponent } from '../sushyst/sushyst.component';
 
 @NgModule({
-  declarations: [VacanciesComponent],
+  declarations: [
+    VacanciesComponent, KuharWokComponent, SushystComponent,
+    KyharRamenComponent, BarmenComponent
+  ],
   imports: [
     CommonModule,
     VacanciesRouterModule,
