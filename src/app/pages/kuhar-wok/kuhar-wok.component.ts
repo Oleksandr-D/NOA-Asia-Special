f853e122b42297fe5e57112339df9fdcf5cc13a2
Oleksandr-D <dolbnja9@gmail.com>
2023-04-12 10:49:58 +0300

    added content at kuhar-wok component

--- src/app/pages/kuhar-wok/kuhar-wok.component.ts
+++ src/app/pages/kuhar-wok/kuhar-wok.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-kuhar-wok',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./kuhar-wok.component.scss']
 })
 export class KuharWokComponent {
+  public questionForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  initAuthForm(): void {
+    this.questionForm = this.fb.group({
+      name: [null, [Validators.required, ]],
+      surname: [null, [Validators.required]],
+      tel: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required]],
+    });
+  }
 }
