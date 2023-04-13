50779e2633a2b188e9286ac6394ce4f013cd867c
Oleksandr-D <dolbnja9@gmail.com>
2023-04-13 10:42:31 +0300

    added content to the barmen component

--- src/app/pages/barmen/barmen.component.ts
+++ src/app/pages/barmen/barmen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-barmen',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./barmen.component.scss']
 })
 export class BarmenComponent {
+  public questionForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  initAuthForm(): void {
+    this.questionForm = this.fb.group({
+      name: [null, [Validators.required, ]],
+      surname: [null, [Validators.required]],
+      tel: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required]],
+    });
+  }
 }
