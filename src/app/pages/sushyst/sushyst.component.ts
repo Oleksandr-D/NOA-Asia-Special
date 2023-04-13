d3bc836c434719f16121dde7362eb41ef69075f5
Oleksandr-D <dolbnja9@gmail.com>
2023-04-13 10:10:05 +0300

    added content to the syshyst component

--- src/app/pages/sushyst/sushyst.component.ts
+++ src/app/pages/sushyst/sushyst.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sushyst',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./sushyst.component.scss']
 })
 export class SushystComponent {
+  public questionForm!: FormGroup;
+  
+  constructor(
+    private fb: FormBuilder
+  ) {}
 
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  initAuthForm(): void {
+    this.questionForm = this.fb.group({
+      name: [null, [Validators.required, ]],
+      surname: [null, [Validators.required]],
+      tel: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required]],
+    });
+  }
 }
