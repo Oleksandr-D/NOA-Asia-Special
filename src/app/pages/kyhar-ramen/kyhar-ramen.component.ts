52ce2a48b07739391aeae65b203839760a533b7f
Oleksandr-D <dolbnja9@gmail.com>
2023-04-14 08:44:22 +0300

    added content to the kyhar-ramen component

--- src/app/pages/kyhar-ramen/kyhar-ramen.component.ts
+++ src/app/pages/kyhar-ramen/kyhar-ramen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-kyhar-ramen',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./kyhar-ramen.component.scss']
 })
 export class KyharRamenComponent {
+  public questionForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initAuthForm();
+  }
+
+  initAuthForm(): void {
+    this.questionForm = this.fb.group({
+      name: [null, [Validators.required, ]],
+      surname: [null, [Validators.required]],
+      tel: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      message: [null, [Validators.required]],
+    });
+  }
 }
