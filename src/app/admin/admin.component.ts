7655033360677aaee98478873533077a2601c3c2
Oleksandr-D <dolbnja9@gmail.com>
2023-04-20 10:08:42 +0300

    added content to the admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+//import { AccountService } from '../shared/services/account/account.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit  {
 
+  constructor(private router: Router, 
+    //private accountService: AccountService
+    ) {}
+
+  ngOnInit(): void {}
+
+  logOut(): void {
+    this.router.navigate(['/']);
+    localStorage.removeItem('currentUser');
+    //this.accountService.isUserLogin$.next(true);
+  }
 }
