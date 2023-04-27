d93749ac83532b9a870dfeb920b967efac70c9ad
Oleksandr-D <dolbnja9@gmail.com>
2023-04-27 11:13:05 +0300

    added content to the product info component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,14 +21,34 @@ export class HomeComponent implements OnInit {
   loadProducts(): void {
     this.productService.getAllByCategoryFirebase('страви-підтримки').then((data) => {
       this.userProducts = data as IProductResponse[];
-      console.log('Works category ==>', this.userProducts);
     });
-    // this.productService.getAllFirebase().subscribe(data => {
-    //   this.userProducts = data as IProductResponse[];
-    //   console.log('Works ==>', this.userProducts);
-    // })
   }
 
+  productCount(product: IProductResponse, value: boolean): void {
+    if (value) {
+      ++product.count;
+    } else if (!value && product.count > 1) {
+      --product.count;
+    }
+  }
+
+  addToBasket(product: IProductResponse): void {
+    let basket: Array<IProductResponse> = [];
+    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+      basket = JSON.parse(localStorage.getItem('basket') as string);
+      if (basket.some((prod) => prod.id === product.id)) {
+        const index = basket.findIndex((prod) => prod.id === product.id);
+        basket[index].count += product.count;
+      } else {
+        basket.push(product);
+      }
+    } else {
+      basket.push(product);
+    }
+    localStorage.setItem('basket', JSON.stringify(basket));
+    product.count = 1;
+    //this.orderService.changeBasket.next(true);
+  }
 
 
   
--- src/app/pages/product/product-info/product-info.component.ts
+++ src/app/pages/product/product-info/product-info.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProductResponse } from 'src/app/shared/interfaces/product/product.interface';
+import { ProductService } from 'src/app/shared/services/product/product.service';
 
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
   styleUrls: ['./product-info.component.scss']
 })
-export class ProductInfoComponent {
+export class ProductInfoComponent implements OnInit {
+  public currentProduct!: IProductResponse;
+
+  constructor(
+    //private orderService:OrderService,
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.getOneProduct();
+  }
+
+  getOneProduct():void{
+    const PRODUCT_ID = this.activatedRoute.snapshot.paramMap.get('id');
+    this.productService.getOneFirebase(PRODUCT_ID as string).subscribe(data =>{
+      this.currentProduct = data as IProductResponse;
+      console.log('=>', this.currentProduct)
+    })
+  }
+
+  productCount(product: IProductResponse, value: boolean): void {
+    if (value) {
+      ++product.count;
+    } else if (!value && product.count > 1) {
+      --product.count;
+    }
+  }
+
+  addToBasket(product: IProductResponse): void {
+    let basket: Array < IProductResponse >= [];
+    if (localStorage.length > 0 && localStorage.getItem('basket')) {
+      basket = JSON.parse(localStorage.getItem('basket') as string);
+      if (basket.some(prod => prod.id === product.id)) {
+        const index = basket.findIndex(prod => prod.id === product.id);
+        basket[index].count += product.count;
+      } else {
+        basket.push(product);
+      }
+    } else {
+      basket.push(product);
+    }
+    localStorage.setItem('basket', JSON.stringify(basket));
+    product.count = 1;
+    //this.orderService.changeBasket.next(true);
+  }
 
 }
