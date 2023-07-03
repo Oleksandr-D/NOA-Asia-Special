ac2ed961b1c9daa84953586c7ca7f5f227acd344
Oleksandr-D <dolbnja9@gmail.com>
2023-07-03 12:58:39 +0300

    added media screen adapt. to admin

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminVacanciesComponent } from "./admin-vacancies/admin-vacancies.component";
 import { AdminProductComponent } from "./admin-product/admin-product.component";
 import { AdminCategoryComponent } from "./admin-category/admin-category.component";
 import { AdminComponent } from "./admin.component";
@@ -12,7 +11,6 @@ const routes: Routes = [
     path: '', component: AdminComponent, children: [
       { path: 'category', component: AdminCategoryComponent },
       { path: 'product', component: AdminProductComponent },
-      { path: 'vacancies', component: AdminVacanciesComponent },
       { path: 'order', component: AdminOrdersComponent },
       { path: 'thai-market', component: AdminThaiMarketComponent  },
       { path: '', pathMatch: 'full', redirectTo: 'category' },
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,7 +5,6 @@ import { SharedModule } from '../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminCategoryComponent } from './admin-category/admin-category.component';
 import { AdminProductComponent } from './admin-product/admin-product.component';
-import { AdminVacanciesComponent } from './admin-vacancies/admin-vacancies.component';
 import { AdminOrdersComponent } from './admin-orders/admin-orders.component';
 import { AdminThaiMarketComponent } from './admin-thai-market/admin-thai-market.component';
 
@@ -14,7 +13,6 @@ import { AdminThaiMarketComponent } from './admin-thai-market/admin-thai-market.
     AdminComponent,
     AdminCategoryComponent,
     AdminProductComponent,
-    AdminVacanciesComponent,
     AdminOrdersComponent,
     AdminThaiMarketComponent
   ],
